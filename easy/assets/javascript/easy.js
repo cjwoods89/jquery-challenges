6313894db7ce13fcaea4aad06e8c09649fad8246
Christopher Woods <Christopher.Woods@LibertyMutual.com>
2017-06-02 10:07:46 -0400

    Initial Commit

--- easy/assets/javascript/easy.js
+++ easy/assets/javascript/easy.js
@@ -15,22 +15,45 @@
    * https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
    */
 
+  myStorage = localStorage;
   var saveButton = $('#save-button');
   var loadButton = $('#load-button');
 
   /**
-   * Gets the text from the element for you
-   * @return {String}
-   */
+  * Gets the text from the element for you
+  * @return {String}
+  */
   function getText() {
-    return $('#save-me').val();
+  return $('#save-me').val();
   }
 
   /**
-   * Puts different text
-   * @param {String} text the stuff you want to put in the box
-   */
+  * Puts different text
+  * @param {String} text the stuff you want to put in the box
+  */
   function setText(text) {
-    return $('#save-me').val(text);
+  return $('#save-me').val(text);
   }
+
+  saveButton.click(function(){
+
+      // console.log('save button pressed');
+
+      myStorage = getText();
+
+      console.log(myStorage);
+
+  });
+
+  loadButton.click(function(){
+
+      // console.log('load button pressed');
+
+      setText(myStorage);
+
+      console.log(myStorage);
+
+
+  });
+
 })();
